b2842ed4117393b9a2690bd84cedfc3811859a84
Linkbal <trananhtuan12a10@gmail.com>
2021-11-27 23:26:50 +0700

    Design detail page

--- resources/views/users/layout/header.blade.php
+++ resources/views/users/layout/header.blade.php
@@ -1,12 +1,28 @@
-<div class="header">
-    <a href="{{ route('user.home') }}" class="logo"><img src="{{ asset('images/logo.jpeg') }}" class="rounded-circle" width="50" height="40"></a>
-    <div class="header-right">
-        <a class="active" href="{{ route('user.home') }}">ホーム</a>
-        <a href="#">会社概要</a>
-        <a href="#">事業内容</a>
-        <a href="#">採用情報</a>
-        <a href="#">間い合わせ</a>
+<div class="header"> 
+    <nav class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand" href="{{ route('user.home') }}"><img src="{{ asset('images/logo.jpeg') }}" class="rounded-circle" width="50" height="40"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="#">ホーム <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">会社概要</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">事業内容</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">採用情報</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">間い合わせ</a>
+        </li>
+        </ul>
     </div>
+    </nav>
 </div>
-<h1 class="text-center">事業内容・ビジネス商品</h1>
-<div style=""><hr></div>
