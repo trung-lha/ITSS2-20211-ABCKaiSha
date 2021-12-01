9074cf5e2349fe8678f6488dfdba47c152837e52
Linkbal <trananhtuan12a10@gmail.com>
2021-12-01 15:45:51 +0700

    Redesign Homepage

--- resources/views/users/layout/slider.blade.php
+++ resources/views/users/layout/slider.blade.php
@@ -4,16 +4,36 @@
         <li data-target="#carouselBanner" data-slide-to="0" class="active"></li>
         <li data-target="#carouselBanner" data-slide-to="1"></li>
         <li data-target="#carouselBanner" data-slide-to="2"></li>
+        <li data-target="#carouselBanner" data-slide-to="3"></li>
     </ol>
     <div class="carousel-inner">
         <div class="carousel-item active">
-        <img class="d-block w-100" src="{{ asset('images/slider-1.jpeg') }}" alt="First slide">
+            <div class="overlay"></div>
+            <img class="d-block w-100" src="{{ asset('images/category-1.jpg') }}">
+            <div class="carousel-caption d-none d-md-block mb-4">
+                <h2>野菜</h2>
+            </div>
         </div>
         <div class="carousel-item">
-        <img class="d-block w-100" src="{{ asset('images/slider-2.jpeg') }}" alt="Second slide">
+            <div class="overlay"></div>
+            <img class="d-block w-100" src="{{ asset('images/category-2.jpg') }}">
+            <div class="carousel-caption d-none d-md-block mb-4">
+                <h2>果物</h2>
+            </div>
         </div>
         <div class="carousel-item">
-        <img class="d-block w-100" src="{{ asset('images/slider-3.jpeg') }}" alt="Third slide">
+            <div class="overlay"></div>
+            <img class="d-block w-100" src="{{ asset('images/category-3.jpg') }}">
+            <div class="carousel-caption d-none d-md-block mb-4">
+                <h2>ジュース</h2>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="overlay"></div>
+            <img class="d-block w-100" src="{{ asset('images/category-4.jpg') }}">
+            <div class="carousel-caption d-none d-md-block mb-4">
+                <h2>ナッツ</h2>
+            </div>
         </div>
     </div>
     <a class="carousel-control-prev" href="#carouselBanner" role="button" data-slide="prev">
