0e2252f7bec7f7c0fb3246babd2f1396e1e456e1
trung-lha <trung.lha100899@gmail.com>
2021-12-04 21:44:30 +0700

    hoan thanh phan hien thi list tuyen dung

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .js('resources/js/slider.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
