8e9285dfe8332eeb1e20ef03e0be1941adfb31c8
Luong Dao Quang Anh <aluong343@gmail.com>
2021-12-26 22:12:01 +0700

    kashagaiyou

--- resources/views/users/company_intro.blade.php
+++ resources/views/users/company_intro.blade.php
@@ -10,7 +10,7 @@
         <div class="col-12 mt-3">
             <form>
                 <div class="form-group row">
-                  <label for="inputEmail3" class="col-sm-2 col-form-label">会社名</label>
+                  <label for="inputEmail3" class="col-sm-2 col-form-label">会社称号</label>
                   <div class="col-sm-10">
                     <input type="text" readonly class="form-control-plaintext" id="" placeholder="なにねねの会社">
                   </div>
@@ -18,40 +18,53 @@
                 <div class="form-group row">
                   <label for="inputEmail3" class="col-sm-2 col-form-label">所存地</label>
                   <div class="col-sm-10">
-                    <input type="text" readonly class="form-control-plaintext" id="" placeholder="１４１−００３１東京都">
+                    <input type="text" readonly class="form-control-plaintext" id="" placeholder="〒141−0031東京都">
                   </div>
                 </div>
                 <div class="form-group row">
-                  <label for="inputEmail3" class="col-sm-2 col-form-label">所存地</label>
+                  <label for="inputEmail3" class="col-sm-2 col-form-label">設立</label>
                   <div class="col-sm-10">
-                    <input type="text" readonly class="form-control-plaintext" id="" placeholder="１４１−００３１東京都">
+                    <input type="text" readonly class="form-control-plaintext" id="" placeholder="2016年11月28日">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label for="inputEmail3" class="col-sm-2 col-form-label">資本金</label>
+                  <div class="col-sm-10">
+                    <input type="text" readonly class="form-control-plaintext" id="" placeholder="344億3,372万円（2020年12月31日現在）">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label for="inputEmail3" class="col-sm-2 col-form-label">代表者</label>
+                  <div class="col-sm-10">
+                    <input type="text" readonly class="form-control-plaintext" id="" placeholder="代表取締役 社長執行役員　掬川 正純">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label for="inputEmail3" class="col-sm-2 col-form-label">従業員数</label>
+                  <div class="col-sm-10">
+                    <input type="text" readonly class="form-control-plaintext" id="" placeholder="連結：7,452名　個別：3,119名（2020年12月31日現在）">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label for="inputEmail3" class="col-sm-2 col-form-label">売上</label>
+                  <div class="col-sm-10">
+                    <input type="text" readonly class="form-control-plaintext" id="" placeholder="連結：3,553億円［IFRS］　個別：2,819億円 （2020年12月期）">
                   </div>
                 </div>
             </form>
         </div>
     </div>
-    <div class="row mt-5 text-center mb-5" style="border: solid 1px black; border-radius: 10px; background-color: rgb(243, 241, 241)">
-      <div class="col-12 mt-3">
-          <form>
-              <div class="form-group row">
-                <label for="inputEmail3" class="col-sm-2 col-form-label">会社名</label>
-                <div class="col-sm-10">
-                  <input type="text" readonly class="form-control-plaintext" id="" placeholder="なにねねの会社">
-                </div>
-              </div>
-              <div class="form-group row">
-                <label for="inputEmail3" class="col-sm-2 col-form-label">所存地</label>
-                <div class="col-sm-10">
-                  <input type="text" readonly class="form-control-plaintext" id="" placeholder="１４１−００３１東京都">
-                </div>
-              </div>
-              <div class="form-group row">
-                <label for="inputEmail3" class="col-sm-2 col-form-label">所存地</label>
-                <div class="col-sm-10">
-                  <input type="text" readonly class="form-control-plaintext" id="" placeholder="１４１−００３１東京都">
-                </div>
-              </div>
-          </form>
+    <div class="row mt-5 mb-5" style="padding: 30px; padding-top: 10px;border: solid 1px black; border-radius: 10px; background-color: rgb(243, 241, 241)">
+      <div class="">
+          <label style="margin-left: 210px">弘前支店</label><br></br>
+          <div>〒036-0233</div>
+          <div>青森県平川市日沼富田19-7</div>
+          <div>TEL 0172-57-5555</div>
+          <div>FAX 0172-57-5577</div>
+          <br>
+          <div>ＪＲ弘前駅より車で15分。</div>
+          <div>東北自動車道黒石ＩＣより弘前方面へ車で15分。</div>
+          </div>
       </div>
   </div>
   </div>
